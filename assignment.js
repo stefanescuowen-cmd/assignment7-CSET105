cfb9739967dd98e5e6bc6e05f7a4148e90a9655d
Owen <stefanescuowen@gmail.com>
2025-10-02 10:08:25 -0400

    Uncommented out color section and commented out the multiplication table section.

--- assignment.js
+++ assignment.js
@@ -1,43 +1,43 @@
 // #1: Multiplication Table //
 
-function createMulti(){
-    let input = Number(window.prompt("Select a number: "))
-    let table = document.getElementById("multiTable")
-    table.innerHTML = ""
+// function createMulti(){
+//     let input = Number(window.prompt("Select a number: "))
+//     let table = document.getElementById("multiTable")
+//     table.innerHTML = ""
 
-    let headerRow = document.createElement("tr")
+//     let headerRow = document.createElement("tr")
 
-    let header1 = document.createElement("th")
-    header1.innerText = "Equation"
+//     let header1 = document.createElement("th")
+//     header1.innerText = "Equation"
 
-    let header2 = document.createElement("th")
-    header2.innerText = "Result"
+//     let header2 = document.createElement("th")
+//     header2.innerText = "Result"
 
-    headerRow.appendChild(header1)
-    headerRow.appendChild(header2)
-    table.appendChild(headerRow)
+//     headerRow.appendChild(header1)
+//     headerRow.appendChild(header2)
+//     table.appendChild(headerRow)
 
-    for(let i=1; i<11; i++){
-        let row = document.createElement("tr")
+//     for(let i=1; i<11; i++){
+//         let row = document.createElement("tr")
 
-        let cell1 = document.createElement("tr")
-        cell1.innerText = `${input} * ${i} = ${input * i}`
+//         let cell1 = document.createElement("tr")
+//         cell1.innerText = `${input} * ${i} = ${input * i}`
 
-        let cell2 = document.createElement("td")
-        cell2.innerText = input * i
+//         let cell2 = document.createElement("td")
+//         cell2.innerText = input * i
 
-        row.appendChild(cell1)
-        row.appendChild(cell2)
-        table.appendChild(row)
-    }
-}
+//         row.appendChild(cell1)
+//         row.appendChild(cell2)
+//         table.appendChild(row)
+//     }
+// }
 
 
 // #2: Colors //
 
-// function whoClickedMe(thing){
-//     console.log(thing);
-//     console.log(thing.id);
-//     console.log(thing.attributes);
-//     console.log(thing.value);
-// }
+function whoClickedMe(thing){
+    console.log(thing);
+    console.log(thing.id);
+    console.log(thing.attributes);
+    console.log(thing.value);
+}
